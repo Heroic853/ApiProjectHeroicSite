b061b17f1ee345154459a6b07db395b97dd23f6f
Amalia Lupu <amalia.lupu@adeo.it>
2026-03-13 12:18:14 +0100

    Upadate01

--- SharedLibrary/Dto/PageVisit.cs
+++ SharedLibrary/Dto/PageVisit.cs
@@ -10,7 +10,7 @@ namespace SharedLibrary.Dto
     {
         public Guid Id { get; set; } 
         public DateTime VisitedAt { get; set; }
-        public string? PageName { get; set; }
+      //  public string? PageName { get; set; }
         public string? UserEmail { get; set; }
     }
 }
